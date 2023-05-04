b5ca5abb3f71258f858a297de08003cacfd8642c
bsponce1 <bsponce1@espe.edu.ec>
2023-05-04 10:05:23 -0500

    ip srv

--- src/database.js
+++ src/database.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 import config from './config'
 
+
 (async () => {
-    const db = await mongoose.connect(config.mongodbURL)
+    const db = await mongoose.set('strictQuery', false).connect(config.mongodbURL)
     console.log('Database is connected to: ',db.connection.name)
 })();
